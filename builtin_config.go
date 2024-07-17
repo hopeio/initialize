5a050386dfc9a6425dbefa4fab5ef4ea6c3edc3d
贾一饼 <Boyang.Liang@apulis.com>
2024-07-18 06:34:07 +0800

    feat: module splitting

---
+++ builtin_config.go
@@ -0,0 +1,7 @@
+package initialize
+
+import "github.com/hopeio/initialize/conf_dao/log"
+
+type BuiltinConfig struct {
+	Log log.Config
+}
---
+++ conf_dao/redis/redis.go
@@ -0,0 +1,57 @@
+package redis
+
+import (
+	"context"
+	"github.com/hopeio/utils/configor"
+	"github.com/hopeio/utils/crypto/tls"
+	"github.com/hopeio/utils/log"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type Config struct {
+	redis.Options
+	CertFile string `json:"cert_file,omitempty"`
+	KeyFile  string `json:"key_file,omitempty"`
+}
+
+func (c *Config) InitBeforeInject() {
+}
+
+func (c *Config) Init() {
+	tlsConfig, err := tls.NewServerTLSConfig(c.CertFile, c.KeyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.TLSConfig = tlsConfig
+	configor.DurationNotify("IdleTimeout", c.IdleTimeout, time.Second)
+}
+
+func (c *Config) Build() (*redis.Client, error) {
+	c.Init()
+	client := redis.NewClient(&c.Options)
+	return client, client.Ping(context.Background()).Err()
+}
+
+type Client struct {
+	*redis.Client
+	Conf Config
+}
+
+func (db *Client) Config() any {
+	return &db.Conf
+}
+
+func (db *Client) Init() error {
+	var err error
+	db.Client, err = db.Conf.Build()
+	return err
+}
+
+func (db *Client) Close() error {
+	if db.Client == nil {
+		return nil
+	}
+	return db.Client.Close()
+}
