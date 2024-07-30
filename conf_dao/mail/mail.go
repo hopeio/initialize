ef3dcc61290a1833444e157f70b796d16da65fa5
贾一饼 <lby.i@qq.com>
2024-07-30 08:33:13 +0800

    fix: mail

--- conf_dao/mail/mail.go
+++ conf_dao/mail/mail.go
@@ -7,22 +7,13 @@ import (
 )
 
 type Config struct {
-	AuthType    string `comment:"CRAMMD5,PLAIN"`
-	PLAINAuth   PlainAuth
-	CRAMMD5Auth CRAMMD5Auth
-}
-
-type PlainAuth struct {
+	AuthType string `comment:"CRAMMD5,PLAIN"`
 	Identity string
 	Host     string
 	Port     string
 	UserName string
 	Password string
-}
-
-type CRAMMD5Auth struct {
-	UserName string
-	Secret   string
+	From     string
 }
 
 func (c *Config) InitBeforeInject() {
@@ -35,12 +26,10 @@ func (c *Config) Init() {
 func (c *Config) Build() (smtp.Auth, error) {
 	c.Init()
 	if strings.ToUpper(c.AuthType) == "PLAIN" {
-		pc := c.PLAINAuth
-		return smtp.PlainAuth(pc.Identity, pc.UserName, pc.Password, pc.Host), nil
+		return smtp.PlainAuth(c.Identity, c.UserName, c.Password, c.Host), nil
 	}
 	if strings.ToUpper(c.AuthType) == "CRAMMD5" {
-		cc := c.CRAMMD5Auth
-		return smtp.CRAMMD5Auth(cc.UserName, cc.Secret), nil
+		return smtp.CRAMMD5Auth(c.UserName, c.Password), nil
 	}
 
 	return nil, errors.New("邮箱配置AuthType必填,PLAIN|CRAMMD5")
