50bd30c3c0619addff15eb7e8b33ebda1ee078b2
贾一饼 <lby.i@qq.com>
2026-03-03 17:05:31 +0800

    fix: test

--- dao/duckdb/duckdb_test.go
+++ dao/duckdb/duckdb_test.go
@@ -11,10 +11,6 @@ import "testing"
 func TestDuckDB(t *testing.T) {
 	config := Config{
 		DSN:         "./duck.db?access_mode=read_only&threads=4",
-		Path:        "",
-		AccessMode:  "",
-		Threads:     0,
-		BootQueries: nil,
 	}
 	db, err := config.Build()
 	if err != nil {
